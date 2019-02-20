711bdf6ba687742c7ee6be674260c57ec97eb402
LLS <2145604059@qq.com>
2019-02-20 19:09:33 +0800

    2019/2/20

--- 面试/InterviewQuestionCode(面试代码)/src/main/java/priv/lint/Tow/LinkedListDome.java
+++ 面试/InterviewQuestionCode(面试代码)/src/main/java/priv/lint/Tow/LinkedListDome.java
@@ -4,7 +4,8 @@ import java.util.LinkedList;
 
 /*
 * LinkedList 就是一个双向链表 ，现对于List就体现出功能的一个加强
-*
+* 这个类提供了链表的一个操作
+* 这个类实现了list和deque接口
 * */
 public class LinkedListDome {
     public static void main(String[] args) {
