af31c19f843bdcf1ec678de20397767fc849d533
LLS <2145604059@qq.com>
2019-02-25 23:51:05 +0800

    2019/2/25

---
+++ 网络编程/TCP/src/main/java/priv/lint/TCPDome/Client.java
@@ -0,0 +1,15 @@
+package priv.lint.TCPDome;
+
+/*
+* TCP协议发送数据步骤：
+* 1，创建发送端的Socket对象 （如果成功，表示连接已经成功）
+* 2，获取输出流，渲染数据
+* 3，关闭流
+* */
+
+import java.net.Socket;
+
+public class Client {
+//    创建发送端的Socket对象
+    Socket socket = new Socket();
+}
