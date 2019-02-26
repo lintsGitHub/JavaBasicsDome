c7480af828bd0cace299e71ce02155316e3f70fb
LLS <2145604059@qq.com>
2019-02-27 00:47:12 +0800

    2019/2/27

--- 网络编程/TCP/src/main/java/priv/lint/TCPDome4/Client.java
+++ 网络编程/TCP/src/main/java/priv/lint/TCPDome4/Client.java
@@ -1,4 +1,14 @@
 package priv.lint.TCPDome4;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+//        建立客户端Socket对象
+        Socket socket = new Socket("192.168.43.98", 6666);
+
+
+
+    }
 }
--- 网络编程/TCP/src/main/java/priv/lint/TCPDome4/Server.java
+++ 网络编程/TCP/src/main/java/priv/lint/TCPDome4/Server.java
@@ -1,4 +1,13 @@
 package priv.lint.TCPDome4;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+//        建立服务端Socket连接对象
+        ServerSocket serverSocket = new ServerSocket(6666);
+
+
+    }
 }
