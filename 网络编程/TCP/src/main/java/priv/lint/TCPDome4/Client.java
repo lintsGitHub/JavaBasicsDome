a6787116865d64c45fe681da7f5bd0fd994b052f
LLS <2145604059@qq.com>
2019-02-27 01:15:53 +0800

    2019/2/27

--- 网络编程/TCP/src/main/java/priv/lint/TCPDome4/Client.java
+++ 网络编程/TCP/src/main/java/priv/lint/TCPDome4/Client.java
@@ -1,13 +1,18 @@
 package priv.lint.TCPDome4;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.Buffer;
 
 public class Client {
     public static void main(String[] args) throws IOException {
 //        建立客户端Socket对象
         Socket socket = new Socket("192.168.43.98", 6666);
 
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        
 
 
     }
